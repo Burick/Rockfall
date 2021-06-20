83196db4b329f91f5a5aae90c244fdefeff7a936
Burick <cyberburick@gmail.com>
2021-06-20 18:58:24 +0300

    Fix SpaceStation.cs

--- Assets/Scripts/SpaceStation.cs
+++ Assets/Scripts/SpaceStation.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SpaceStation : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        IndicatorManager.instance.AddIndicator(
+            gameObject,
+            Color.green
+        );
     }
 }
