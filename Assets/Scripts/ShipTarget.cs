260ab535df707290fb292f4d9a2c18491b885235
Burick <cyberburick@gmail.com>
2021-06-22 23:26:03 +0300

    Add

---
+++ Assets/Scripts/ShipTarget.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipTarget : MonoBehaviour {
+// Спрайт для использования в качестве прицельной сетки.
+    [Header("Спрайт прицельной сетки")]
+    public Sprite targetImage;
+    void Start () {
+// Зарегистрировать новый индикатор, соответствующий
+// данному объекту, использовать желтый цвет и
+// нестандартный спрайт.
+        IndicatorManager.instance.AddIndicator(gameObject,
+            Color.yellow, targetImage);
+    }
+}
